c9893641b625a16cb39349d81ee60e17e0441dd4
Eelco Wiersma <eelco@appulse.nl>
2024-12-26 12:29:52 +0000

    chore: update content schema

--- website/content-collections.ts
+++ website/content-collections.ts
@@ -8,9 +8,9 @@ const docs = defineCollection({
   schema: (z) => ({
     title: z.string(),
     description: z.string(),
-    icon: z.string(),
-    full: z.boolean(),
-    _openapi: z.record(z.string(), z.any()),
+    icon: z.string().optional(),
+    full: z.boolean().optional(),
+    _openapi: z.record(z.string(), z.any()).optional(),
   }),
   transform: transformMDX,
 })
@@ -21,12 +21,12 @@ const metas = defineCollection({
   include: '**/meta.json',
   parser: 'json',
   schema: (z) => ({
-    title: z.string(),
-    description: z.string(),
-    pages: z.array(z.string()),
-    icon: z.string(),
-    root: z.boolean(),
-    defaultOpen: z.boolean(),
+    title: z.string().optional(),
+    description: z.string().optional(),
+    pages: z.array(z.string()).optional(),
+    icon: z.string().optional(),
+    root: z.boolean().optional(),
+    defaultOpen: z.boolean().optional(),
   }),
 })
 
