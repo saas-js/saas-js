c3daca4978162e8a2258a6b2a25e0e9acf6d1b7a
Eelco Wiersma <eelco@appulse.nl>
2024-12-26 12:27:18 +0000

    fix: dics

--- website/content-collections.ts
+++ website/content-collections.ts
@@ -1,26 +1,35 @@
-import { defineCollection, defineConfig } from '@content-collections/core';
-import {
-  createMetaSchema,
-  createDocSchema,
-  transformMDX,
-} from '@fumadocs/content-collections/configuration';
+import { defineCollection, defineConfig, z } from '@content-collections/core'
+import { transformMDX } from '@fumadocs/content-collections/configuration'
 
 const docs = defineCollection({
   name: 'docs',
   directory: 'content/docs',
   include: '**/*.mdx',
-  schema: createDocSchema,
+  schema: (z) => ({
+    title: z.string(),
+    description: z.string(),
+    icon: z.string(),
+    full: z.boolean(),
+    _openapi: z.record(z.string(), z.any()),
+  }),
   transform: transformMDX,
-});
+})
 
 const metas = defineCollection({
   name: 'meta',
   directory: 'content/docs',
   include: '**/meta.json',
   parser: 'json',
-  schema: createMetaSchema,
-});
+  schema: (z) => ({
+    title: z.string(),
+    description: z.string(),
+    pages: z.array(z.string()),
+    icon: z.string(),
+    root: z.boolean(),
+    defaultOpen: z.boolean(),
+  }),
+})
 
 export default defineConfig({
   collections: [docs, metas],
-});
+})
