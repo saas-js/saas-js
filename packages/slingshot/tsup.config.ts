52571fbff12c4d88f14ef60f3b8e84df25aa0c9e
Eelco Wiersma <eelco@appulse.nl>
2025-07-09 12:17:40 +0100

    chore: ignore slingshot build

--- packages/slingshot/tsup.config.ts
+++ packages/slingshot/tsup.config.ts
@@ -2,11 +2,11 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
   entry: [
-    'src/index.ts',
-    'src/client.ts',
-    'src/next/index.ts',
-    'src/remix-node/index.ts',
-    'src/aws-lambda/index.ts',
+    './src/index.ts',
+    './src/client.ts',
+    // './src/next/index.ts',
+    // './src/remix-node/index.ts',
+    // './src/aws-lambda/index.ts',
   ],
   splitting: false,
   sourcemap: false,
