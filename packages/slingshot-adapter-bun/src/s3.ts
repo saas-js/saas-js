9c5d8afaa1750cf30406516641d98da8a1026e2c
Eelco Wiersma <eelco@appulse.nl>
2026-03-07 20:47:49 +0100

    feat: forward bun s3 options

--- packages/slingshot-adapter-bun/src/s3.ts
+++ packages/slingshot-adapter-bun/src/s3.ts
@@ -1,8 +1,15 @@
-import { s3 } from 'bun'
+import { type S3FileOptions, s3 } from 'bun'
 
 import type { SlingshotAdapter } from '@saas-js/slingshot'
 
 import type { CreateSignedUrlArgs } from './types.ts'
+
+export interface BunS3AdapterArgs
+  extends CreateSignedUrlArgs,
+    Omit<
+      S3FileOptions,
+      'bucket' | 'region' | 'accessKeyId' | 'secretAccessKey'
+    > {}
 
 export const createSignedUrl = ({
   credentials,
@@ -11,18 +18,19 @@ export const createSignedUrl = ({
   key,
   method = 'PUT',
   expiresIn = 3600,
-}: CreateSignedUrlArgs) => {
+  ...options
+}: BunS3AdapterArgs) => {
   const file = s3(key, {
     bucket,
     region,
     accessKeyId: credentials.accessKeyId,
     secretAccessKey: credentials.secretAccessKey,
+    ...options,
   })
 
   return file.presign({
     method,
-    endpoint: `https://${bucket}.s3.${region}.amazonaws.com`,
-    expiresIn: expiresIn,
+    expiresIn,
   })
 }
 
