be24e7a64b7b69fc9925bff7b2fe613b7e74b2b1
Eelco Wiersma <eelco@appulse.nl>
2026-03-07 21:17:23 +0100

    chore: release

--- packages/slingshot-adapter-bun/src/s3.ts
+++ packages/slingshot-adapter-bun/src/s3.ts
@@ -1,12 +1,15 @@
 import { type S3Options, s3 } from 'bun'
 
-import type { SlingshotAdapter } from '@saas-js/slingshot'
+import type { SlingshotAdapter, SlingshotAdapterArgs } from '@saas-js/slingshot'
 
 import type { CreateSignedUrlArgs } from './types.ts'
 
-export interface BunS3AdapterArgs
-  extends CreateSignedUrlArgs,
-    Omit<S3Options, 'bucket' | 'region' | 'accessKeyId' | 'secretAccessKey'> {}
+type BunS3Options = Omit<
+  S3Options,
+  'bucket' | 'region' | 'accessKeyId' | 'secretAccessKey'
+>
+
+export interface BunS3AdapterArgs extends SlingshotAdapterArgs, BunS3Options {}
 
 export const createSignedUrl = ({
   credentials,
@@ -16,7 +19,7 @@ export const createSignedUrl = ({
   method = 'PUT',
   expiresIn = 3600,
   ...options
-}: BunS3AdapterArgs) => {
+}: CreateSignedUrlArgs & BunS3Options) => {
   const file = s3.file(key, {
     bucket,
     region,
